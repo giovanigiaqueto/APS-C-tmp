071fe16d84418ae56b9e62b47893950dde95a04f
Giovani Giaqueto de Oliveira <giovanigiaqueto@gmail.com>
2021-10-18 21:38:04 -0300

    Algoritimo Quick Sort (implementação não-recusiva)
    a implementação recusiva não funcionou
    com entradas muito grandes por causa de
    um estouro da pilha causada por recursão.
    
    essa implementação substitui a recursão
    por um loop e uma pilha com os argumentos
    da função, podendo lidar com entradas maiores.

---
+++ src/quick_sort.c
@@ -0,0 +1,106 @@
+
+#include "quick_sort.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+struct ValorPilha {
+	unsigned int       esquerda;
+	unsigned int       direita;
+	struct ValorPilha* proximo;
+};
+typedef struct ValorPilha ValorPilha;
+
+typedef struct {
+	struct ValorPilha* valores;
+} Pilha;
+
+int __ordenar_quick_sort(char** linhas, unsigned int esquerda, unsigned int direita) {
+	unsigned int i = esquerda;
+	unsigned int j = direita;
+
+	Pilha pilha;
+
+	ValorPilha* _valor = malloc(sizeof(ValorPilha));
+	if (_valor == NULL) return -1;
+
+	_valor->esquerda = esquerda;
+	_valor->direita  = direita;
+	_valor->proximo  = NULL;
+
+	pilha.valores = _valor;
+
+	while (pilha.valores) {
+		ValorPilha* valor = pilha.valores;
+
+		esquerda = valor->esquerda;
+		direita  = valor->direita;
+
+		pilha.valores = valor->proximo;
+		free((void*) valor);
+
+		char* x = linhas[(esquerda + direita) / 2];
+
+		while (i <= j) {
+			while (strcmp(linhas[i], x) < 0 && i < direita) {
+				i++;
+			}
+			while (strcmp(linhas[j], x) > 0 && j > esquerda) {
+				j--;
+			}
+			if (i <= j) {
+				char* tmp = linhas[i];
+				linhas[i] = linhas[j];
+				linhas[j] = tmp;
+				i++;
+				j--;
+			}
+		}
+
+		if (j > esquerda) {
+			valor = malloc(sizeof(ValorPilha));
+			if (valor == NULL) {
+				valor = pilha.valores;
+				while (valor != NULL) {
+					ValorPilha* proximo = valor->proximo;
+					free((void*) valor);
+					valor = proximo;
+				}
+
+				return -1;
+			}
+
+			valor->esquerda = esquerda;
+			valor->direita  = j;
+			valor->proximo  = pilha.valores;
+
+			pilha.valores = valor;
+		}
+		if (i < direita) {
+			valor = malloc(sizeof(ValorPilha));
+			if (valor == NULL) {
+				valor = pilha.valores;
+				while (valor != NULL) {
+					ValorPilha* proximo = valor->proximo;
+					free((void*) valor);
+					valor = proximo;
+				}
+
+				return -1;
+			}
+
+			valor->esquerda = i;
+			valor->direita  = direita;
+			valor->proximo  = pilha.valores;
+
+			pilha.valores = valor;
+		}
+	}
+
+	return 0;
+}
+
+int ordenar_quick_sort(char** linhas, unsigned int qtd_linhas) {
+	return __ordenar_quick_sort(linhas, 0, qtd_linhas - 1);
+}
---
+++ src/quick_sort.h
@@ -0,0 +1,6 @@
+#ifndef __ORDENAR_QUICK_SORT_H
+#define __ORDENAR_QUICK_SORT_H
+
+int ordenar_quick_sort(char** linhas, unsigned int qtd_linhas);
+
+#endif
