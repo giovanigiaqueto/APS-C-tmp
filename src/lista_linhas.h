e3107098904d10aa7b0c92bfbfd4db0b3e3190eb
Giovani Giaqueto de Oliveira <giovanigiaqueto@gmail.com>
2021-11-02 20:38:51 -0300

    Documentação do módulo de lista de linhas
    documentação básica, descrevendo o que cada
    função do módulo de lista de linhas faz.

--- src/lista_linhas.h
+++ src/lista_linhas.h
@@ -10,14 +10,20 @@ typedef struct ListaLinhas {
 	ListaLinhas_No* base;
 } ListaLinhas;
 
+// aloca e inicializa uma lista de linhas na heap
 ListaLinhas* criar_lista_linhas();
 
+// finaliza e libera uma lista de linhas alocada na heap
 void deletar_lista_linhas(ListaLinhas* lista);
 
+// insere uma linha na lista de linhas
 int inserir_lista_linhas(ListaLinhas_No** base, char* linha);
 
+// remove uma linha da lista de linhas
 int remover_lista_linhas(ListaLinhas_No** base);
 
+// move uma linha dentro de uma lista de
+// linhas ou entre duas lista de linhas
 int mover_lista_linhas(ListaLinhas_No** dest, ListaLinhas_No** orig);
 
 #endif
